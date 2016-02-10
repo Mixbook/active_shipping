7212a5a65f269f52e76b1ba258b75211867b5aba
Vitalie Lazu <vitalie.lazu@gmail.com>
2016-02-10 11:03:48 +0200

    Fixed gem dependencies to be able to run tests with ruby 1.9.3-p551

--- active_shipping.gemspec
+++ active_shipping.gemspec
@@ -16,14 +16,15 @@ Gem::Specification.new do |s|
   s.required_rubygems_version = ">= 1.3.6"
   s.rubyforge_project         = "active_shipping"
 
-  s.add_dependency('activesupport', '>= 2.3.5')
+  s.add_dependency('activesupport', '~> 3.2')
   s.add_dependency('i18n')
-  s.add_dependency('active_utils', '>= 1.0.1')
+  s.add_dependency('active_utils', '~> 2.2.0')
   s.add_dependency('builder')
   s.add_dependency('json', '>= 1.5.1')
 
+  s.add_development_dependency('minitest', '~> 4.7.5')
   s.add_development_dependency('rake')
-  s.add_development_dependency('mocha')
+  s.add_development_dependency('mocha', '~> 0.14.0')
 
   s.files        = Dir.glob("lib/**/*") + %w(MIT-LICENSE README.markdown CHANGELOG)
   s.require_path = 'lib'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,8 +7,13 @@ Bundler.setup
 
 require 'test/unit'
 require 'active_shipping'
-require 'mocha'
+require 'mocha/setup'
 
+# This makes sure that Minitest::Test exists when an older version of Minitest
+# (i.e. 4.x) is required by ActiveSupport.
+unless defined?(Minitest::Test)
+  Minitest::Test = MiniTest::Unit::TestCase
+end
 
 XmlNode # trigger autorequire
 
@@ -16,52 +21,52 @@ module Test
   module Unit
     class TestCase
       include ActiveMerchant::Shipping
-      
+
       LOCAL_CREDENTIALS = ENV['HOME'] + '/.active_merchant/fixtures.yml' unless defined?(LOCAL_CREDENTIALS)
       DEFAULT_CREDENTIALS = File.dirname(__FILE__) + '/fixtures.yml' unless defined?(DEFAULT_CREDENTIALS)
-      
+
       MODEL_FIXTURES = File.dirname(__FILE__) + '/fixtures/' unless defined?(MODEL_FIXTURES)
 
       def all_fixtures
         @@fixtures ||= load_fixtures
       end
-      
+
       def fixtures(key)
         data = all_fixtures[key] || raise(StandardError, "No fixture data was found for '#{key}'")
-        
+
         data.dup
       end
-          
+
       def load_fixtures
         file = File.exists?(LOCAL_CREDENTIALS) ? LOCAL_CREDENTIALS : DEFAULT_CREDENTIALS
         yaml_data = YAML.load(File.read(file))
-        
+
         model_fixtures = Dir.glob(File.join(MODEL_FIXTURES,'**','*.yml'))
         model_fixtures.each do |file|
           name = File.basename(file, '.yml')
           yaml_data[name] = YAML.load(File.read(file))
         end
-        
+
         symbolize_keys(yaml_data)
-      
+
         yaml_data
       end
-      
+
       def xml_fixture(path) # where path is like 'usps/beverly_hills_to_ottawa_response'
         open(File.join(File.dirname(__FILE__),'fixtures','xml',"#{path}.xml")) {|f| f.read}
       end
-      
+
       def json_fixture(path) # where path is like 'usps/beverly_hills_to_ottawa_response'
         open(File.join(File.dirname(__FILE__),'fixtures','json',"#{path}.json")) {|f| f.read}
       end
-      
+
       def symbolize_keys(hash)
         return unless hash.is_a?(Hash)
-        
+
         hash.symbolize_keys!
         hash.each{|k,v| symbolize_keys(v)}
       end
-      
+
     end
   end
 end
@@ -69,9 +74,9 @@ end
 module ActiveMerchant
   module Shipping
     module TestFixtures
-      
+
       mattr_reader :packages, :locations
-      
+
       @@packages = {
         :just_ounces => Package.new(16, nil, :units => :imperial),
         :just_grams => Package.new(1000, nil),
@@ -90,7 +95,7 @@ module ActiveMerchant
         :shipping_container => Package.new(2200000, [2440, 2600, 6058], :description => '20 ft Standard Container', :units => :metric),
         :largest_gold_bar => Package.new(250000, [ 45.5, 22.5, 17 ], :value => 15300000)
       }
-      
+
       @@locations = {
         :bare_ottawa => Location.new(:country => 'CA', :postal_code => 'K1P 1J1'),
         :bare_beverly_hills => Location.new(:country => 'US', :zip => '90210'),
@@ -196,7 +201,7 @@ module ActiveMerchant
                                       :address1 => '192 Victoria St West',
                                       :postal_code => '1010')
       }
-      
+
     end
   end
 end
